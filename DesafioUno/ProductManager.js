6db93ae17819e3b0f10b2eaa71c658e4f7e5f78a
GonzaLorenzo <gonzalodlorenzo@gmail.com>
2023-07-25 19:20:19 -0300

    Descripcion

--- DesafioUno/ProductManager.js
+++ DesafioUno/ProductManager.js
@@ -1,8 +1,8 @@
 class ProductManager
 {
-    constructor(products = []) 
+    constructor() 
     {
-        this.products = products;
+        this.products = [];
         this.GeneratedID = 0;
     }
 
@@ -42,7 +42,7 @@ class ProductManager
 
     getProductByID(id)
     {
-        const product = this.products.find(product => product.id == id)
+        let product = this.products.find(product => product.id == id)
         if(product)
         {
             return product;
@@ -54,11 +54,23 @@ class ProductManager
     }
 }
 
+//Creo la instancia de Product Manager.
 const productManager = new ProductManager();
 
+//Llamo a getProducts para que devuelva el array vacio.
 console.log(productManager.getProducts());
+
+//Llamo a addProduct y añado el producto prueba.
 productManager.addProduct("producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25);
+
+//Obtengo el array con el producto recien agregado.
 console.log(productManager.getProducts());
+
+//Intengo añadir un producto con el mismo code, no funciona.
 productManager.addProduct("producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25);
+
+//Intento obtener un producto con un ID existente.
 console.log(productManager.getProductByID(0));
+
+//Intento obtener un producto con un ID inexistente.
 console.log(productManager.getProductByID(32));
