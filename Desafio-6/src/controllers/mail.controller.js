1f39be812a5b965bf574558a54c362410053fb44
GonzaLorenzo <gonzalodlorenzo@gmail.com>
2023-10-31 14:08:39 -0300

    Mailing con Nodemailer

---
+++ Desafio-6/src/controllers/mail.controller.js
@@ -0,0 +1,45 @@
+import nodemailer from 'nodemailer';
+import { __dirname } from '../path.js';
+
+let transporter = nodemailer.createTransport(
+    {
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: 
+        {
+            user: 'gonzalodlorenzoprueba@gmail.com',
+            pass: process.env.PASSWORD_EMAIL,
+            authMethod: 'LOGIN',
+        },
+    }
+)
+
+const sendEmail = async (req, res) => 
+{
+	const resultado = await transporter.sendMail(
+        {
+            from: 'Backend47280Test gonzalodlorenzoprueba@gmail.com',
+            to: 'gonzalodlorenzo@gmail.com',
+            subject: 'nodemailerTest',
+            html: 
+            `
+            <div>
+                <h1>Probando</h1>
+            </div>
+            `,
+            attachments: [
+                {
+                    filename: 'peacewalker.jpg',
+                    path: __dirname + '/images/peacewalker.jpg',
+                    cid: 'peacewalker.jpg',
+                },
+            ],
+	    }
+    )
+	res.send({ message: 'Mail enviado', response: resultado })
+}
+
+const mailingController = { sendEmail }
+
+export default mailingController
--- Desafio-6/src/routes/index.routes.js
+++ Desafio-6/src/routes/index.routes.js
@@ -3,6 +3,8 @@ import routerCart from "./carts.routes.js";
 import routerProd from "./products.routes.js";
 import routerSession from "./sessions.routes.js";
 import routerUser from "./users.routes.js";
+import routerMailing from "./mail.routes.js";
+
 
 const router = Router()
 
@@ -10,5 +12,6 @@ router.use('/api/products', routerProd)
 router.use('/api/users', routerUser)
 router.use('/api/carts', routerCart)
 router.use('/api/sessions', routerSession)
+router.use('/api/mail', routerMailing);
 
 export default router
